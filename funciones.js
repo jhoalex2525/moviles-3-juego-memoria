ac99cda6c9ec5099565081e1e3753a0485d441d8
308 <308@academicos.cesde.edu.co>
2022-02-15 20:37:36 -0500

    inicio de proyecto de juego de memoria

---
+++ funciones.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let imagenes = [
+        {   img:"images/arceus.jpg",
+            name:"arceus"
+        },
+        {   img:"images/bulbasaur.jpg",
+            name:"bulbasaur"
+        },
+        {   img:"images/charmander.jpg",
+            name:"charmander"
+        },
+        {   img:"images/pikachu.jpg",
+            name:"pikachu"
+        },
+        {   img:"images/serperior.jpg",
+            name:"serperior"
+        },
+        {   img:"images/squirtle.jpg",
+            name:"squirtle"
+        },
+        {   img:"images/arceus.jpg",
+            name:"arceus"
+        },
+        {   img:"images/bulbasaur.jpg",
+            name:"bulbasaur"
+        },
+        {   img:"images/charmander.jpg",
+            name:"charmander"
+        },
+        {   img:"images/pikachu.jpg",
+            name:"pikachu"
+        },
+        {   img:"images/serperior.jpg",
+            name:"serperior"
+        },
+        {   img:"images/squirtle.jpg",
+            name:"squirtle"
+        }        
+    ]
+});
